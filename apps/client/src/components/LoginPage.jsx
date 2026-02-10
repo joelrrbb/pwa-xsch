4019074fe88a1650f34daa1e4df84f3dda2bf053
joelrrbb <rrodolfojoel@gmail.com>
2026-02-10 11:53:29 -0400

    feat: update login

--- apps/client/src/components/LoginPage.jsx
+++ apps/client/src/components/LoginPage.jsx
@@ -134,6 +134,8 @@ const LoginPage = ({ onLogin }) => {
                 />
               </div>
             </IonItem>
+			
+			<div style={{ padding: '0 .9rem' }}>
 
             <div style={{ textAlign: 'center', marginBottom: '30px' }}>
               <IonText className="ys-text">
@@ -192,6 +194,9 @@ const LoginPage = ({ onLogin }) => {
               {!loading ? 'Ingresar' : <IonSpinner name="crescent" color="light" />}
             </IonButton>
           </div>
+		  
+		  </div>
+		  
         </div>
       </IonContent>
     </IonPage>
