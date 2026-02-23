b9e278b99180e69a305f45ce12c48b3911438d76
joelrrbb <rrodolfojoel@gmail.com>
2026-02-23 01:48:32 -0400

    add date

--- apps/client/src/components/ReferidosPage.jsx
+++ apps/client/src/components/ReferidosPage.jsx
@@ -407,8 +407,8 @@ const ReferidosPage = () => {
         <IonModal
           isOpen={showDatePicker}
           onDidDismiss={() => setShowDatePicker(false)}
-          initialBreakpoint={0.355}
-          breakpoints={[0, 0.355, 0.6]}
+          initialBreakpoint={0.335}
+          breakpoints={[0, 0.335, 0.6]}
         >
           <IonHeader className="ion-no-border">
             <IonToolbar>
